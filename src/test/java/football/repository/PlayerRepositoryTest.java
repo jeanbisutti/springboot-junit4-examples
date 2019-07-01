91190efa7452dea13ba8d1bb12988820e59e7838
Jean Bisutti <jean.bisutti@gmail.com>
2019-07-01 20:56:43 +0200

    Add documentation for @FunctionalIteration

--- src/test/java/football/repository/PlayerRepositoryTest.java
+++ src/test/java/football/repository/PlayerRepositoryTest.java
@@ -4,6 +4,8 @@ import football.QuickPerfPerBeanConfig;
 import football.entity.Player;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.quickperf.annotation.DisableQuickPerf;
+import org.quickperf.annotation.FunctionalIteration;
 import org.quickperf.spring.junit4.QuickPerfSpringRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -21,9 +23,16 @@ public class PlayerRepositoryTest {
     @Autowired
     private PlayerRepository playerRepository;
 
-    // The test will fail because N+1 select is detected
-    // from disableSameSelectTypesWithDifferentParams defined
-    // in QuickPerfConfiguration
+    //@FunctionalIteration // You can use @FunctionalIteration or @DisableQuickPerf
+                           // to disable QuickPerf features and focus on functional
+                           // behavior (not performance behavior) in a first step.
+                           // In a second step, you can remove @FunctionalIteration
+                           // or @DisableQuickPerf to evaluate some performance
+                           // properties.
+                           // We recommend to do one step at a time.
+    // The test will fail because N+1 select is detected from
+    // disableSameSelectTypesWithDifferentParams defined in QuickPerfConfiguration
+    // class.
     @Test
     public void should_find_all_players() {
         List<Player> players = playerRepository.findAll();
